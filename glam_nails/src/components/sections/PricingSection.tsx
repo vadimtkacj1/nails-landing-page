ecbe0efdf044e2157a7d00474ff1b33ecbe706a6
Vadim Tkachenko <vadim.tkach1378@gmail.com>
2026-05-28 11:41:59 +0300

    changed picture

--- glam_nails/src/components/sections/PricingSection.tsx
+++ glam_nails/src/components/sections/PricingSection.tsx
@@ -48,7 +48,7 @@ const PricingRow = ({ title, price, currency }: PricingItem & { currency: string
 const BASE = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
 
 const CATEGORY_META: { id: string; image: string; imagePosition: 'left' | 'right' }[] = [
-  { id: 'permanent', image: `${BASE}/images/img_image_618x460.png`, imagePosition: 'left' },
+  { id: 'permanent', image: `${BASE}/images/photos/photo_48.jpeg`, imagePosition: 'left' },
   { id: 'manicure', image: `${BASE}/images/img_image_2.png`, imagePosition: 'right' },
   { id: 'pedicure', image: `${BASE}/images/img_image_400x558.png`, imagePosition: 'left' },
 ];
